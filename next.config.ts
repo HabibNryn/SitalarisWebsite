983b73c92e136cbb01739834f524820944b1ffe9
Habib Andriyan <habibadrian25@gmail.com>
2026-01-26 09:44:48 +0700

    error deployment

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,13 @@
 // next.config.ts
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
 
-  webpack: (config, { isServer }) => {
-
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname),
-    };
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        path: false,
-        stream: false,
-        crypto: false,
-        os: false,
-      };
-    }
-
-    return config;
+  turbopack: {
+    resolveAlias: {
+      "@": "./",
+    },
   },
 
   experimental: {
